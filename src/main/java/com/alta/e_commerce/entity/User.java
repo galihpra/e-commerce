872879e2f01fb8c4bc750b35c61749d6a60796c1
Galih Prayoga <galihp83@gmail.com>
2024-06-13 18:17:21 +0700

    user entity add username

--- src/main/java/com/alta/e_commerce/entity/User.java
+++ src/main/java/com/alta/e_commerce/entity/User.java
@@ -21,6 +21,9 @@ public class User {
     @Column(unique = true)
     private String email;
 
+    @Column(unique = true)
+    private String username;
+
     private String password;
 
     private String name;
