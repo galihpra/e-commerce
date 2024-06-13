4e33103296617691aec19d57aef123f33add3e1c
Galih Prayoga <galihp83@gmail.com>
2024-06-13 18:25:52 +0700

    user repository

---
+++ src/main/java/com/alta/e_commerce/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alta.e_commerce.repository;
+
+import com.alta.e_commerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
+    User findByEmail(String email);
+}
