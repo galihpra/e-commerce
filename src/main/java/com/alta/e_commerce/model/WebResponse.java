a882d043920481e959c806571d60826255cd0860
Galih Prayoga <galihp83@gmail.com>
2024-06-15 13:00:16 +0700

    web response

---
+++ src/main/java/com/alta/e_commerce/model/WebResponse.java
@@ -0,0 +1,42 @@
+package com.alta.e_commerce.model;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+/**
+ * {
+ *     "message":"success/failed",
+ *     "data": ...
+ * }
+ *
+ * {
+ *  *         "meta":{
+ *  *             "status":"ok",
+ *  *             "message":""
+ *  *         },
+ *  *         "data":{
+ *  *             ....
+ *  *         }
+ *  *     }
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private PaginationResponse pagination;
+
+}
+
