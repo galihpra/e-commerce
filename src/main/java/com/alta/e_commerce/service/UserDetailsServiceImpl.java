7739310dc70223df5f1697aa9576c30a893f7279
Galih Prayoga <galihp83@gmail.com>
2024-06-15 12:36:05 +0700

    user dtails implementation

---
+++ src/main/java/com/alta/e_commerce/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.alta.e_commerce.service;
+
+import com.alta.e_commerce.entity.User;
+import com.alta.e_commerce.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+
+        User user = repository.findByEmail(email);
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
+
